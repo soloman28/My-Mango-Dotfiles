46ee3d254c229e6013bf0eed6e986d6b6547743a
soloman28 <the.s0l0man.2004@gmail.com>
2026-08-15 17:47:28 +0800

    Making my own install script...

--- install.sh
+++ install.sh
@@ -2,94 +2,92 @@
 
 clear
 
-if [[ $EUID -ne 0 ]]; then
-    echo "Root is needed to install dependencies. Please run as superuser."
-    exit 1
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Uh uh uh! You need to run as a root!"
+    exit
 fi
 
 if [[ -z "$SUDO_USER" || "$SUDO_USER" == "root" ]]; then
-    echo "Please run this script with sudo."
-    exit 1
+    echo "Uh uh uh! You have to run this script with sudo!"
+    exit
 fi
 
 USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
 
 if [[ -z "$USER_HOME" || ! -d "$USER_HOME" ]]; then
-    echo "Could not determine the user's home directory."
-    exit 1
+    echo "Whoops! This could not determine the user's home directory, could it?"
+    exit
 fi
 
-selectedPackageManager=""
+packageManager=""
 
 yay=false
-paru=false
 
-echoLogo() {
-    echo "======================================="
-    echo "===========| My Mango Dots |==========="
-    echo "======================================="
+echoIntro () {
+    echo "Howdy! Some of these packages require yay or manual install."
+    echo "If you're okay with it, would you like to install it?"
+    echo "This requires an internet connection."
 }
 
-getPackageManager() {
+packageManagerCheck() {
     clear
-    echoLogo
+    echoIntro
     echo
-    echo "Both Yay and Paru are installed."
-    echo "Which one should be used?"
-    echo
-    echo "[y] Yay"
-    echo "[p] Paru"
-    echo
+    echo "[Y] Yes!"
+    echo "[N] No!"
 
-    read -r packageManager
+    read -r installPackageManager
 
-    case "$packageManager" in
-        y|yay|Y|YAY)
-            selectedPackageManager="yay"
-            yayInstallPackages
-            ;;
-        p|paru|P|PARU)
-            selectedPackageManager="paru"
-            paruInstallPackages
+    case "$installPackageManager" in
+        y|yes|Y|YES)
+            installPackageManager
+        ;;
+        n|no|N|NO)
+            clear
+            echo "Alright, better luck next time!"
+            exit
             ;;
         *)
             echo
-            echo "Wrong input."
+            echo "Oops! Wrong input!"
             sleep 1
             getPackageManager
             ;;
     esac
 }
 
-installPackageManagerParu() {
+getPackageManager() {
     clear
-    echoLogo
+    echo "Looks like Yay is installed!"
+    echo "Are you sure you're going to install packages?"
     echo
-    echo "Installing Paru..."
-    echo
+    echo "[Y] Yes!"
+    echo "[N] No!"
 
-    pacman -S --needed --noconfirm git base-devel
+    read -r packageManager
 
-    if [[ ! -d "$USER_HOME/paru" ]]; then
-        sudo -u "$SUDO_USER" git clone https://aur.archlinux.org/paru.git "$USER_HOME/paru"
-    fi
-
-    cd "$USER_HOME/paru" || exit 1
-
-    sudo -u "$SUDO_USER" makepkg -si --noconfirm
-
-    cd "$USER_HOME" || exit 1
-    rm -rf "$USER_HOME/paru"
-
-    clear
-    echoLogo
+    case "$packageManager" in
+        y|yes|Y|YES)
+            installPackages
+        ;;
+        n|no|N|NO)
+            clear
+            echo "Alright, better luck next time!"
+            exit
+            ;;
+        *)
+            echo
+            echo "Oops! Wrong input!"
+            sleep 1
+            getPackageManager
+            ;;
+    esac
 }
 
-installPackageManagerYay() {
+
+installPackageManager() {
     clear
-    echoLogo
-    echo
-    echo "Installing Yay..."
+    echo "Yay going up!"
     echo
 
     pacman -S --needed --noconfirm git base-devel
@@ -106,54 +104,11 @@ installPackageManagerYay() {
     rm -rf "$USER_HOME/yay-bin"
 
     clear
-    echoLogo
 }
 
-installPackageManagerCheck() {
+installPackages() {
     clear
-    echoLogo
-    echo
-    echo "Could not find Yay or Paru."
-    echo "Do you want to install one?"
-    echo
-    echo "[y] Yay"
-    echo "[p] Paru"
-    echo "[n] No"
-    echo
-
-    read -r installPackageManager
-
-    case "$installPackageManager" in
-        y|yay|Y|YAY)
-            installPackageManagerYay
-            yayInstallPackages
-            ;;
-        p|paru|P|PARU)
-            installPackageManagerParu
-            paruInstallPackages
-            ;;
-        n|no|N|NO|No)
-            clear
-            echoLogo
-            echo
-            echo "Not installing a package manager."
-            echo "A package manager is required. Exiting..."
-            exit 1
-            ;;
-        *)
-            echo
-            echo "Wrong input."
-            sleep 1
-            installPackageManagerCheck
-            ;;
-    esac
-}
-
-yayInstallPackages() {
-    clear
-    echoLogo
-    echo
-    echo "Installing packages with Yay..."
+    echo "Installing packages with Yay going up!"
     echo
 
     sudo -u "$SUDO_USER" yay -Sy --noconfirm \
@@ -190,61 +145,11 @@ yayInstallPackages() {
 
     if [[ $? -ne 0 ]]; then
         echo
-        echo "Package installation failed."
-        exit 1
+        echo "Whoops, package installation failed!"
+        exit
     fi
 
     clear
-    echoLogo
-}
-
-paruInstallPackages() {
-    clear
-    echoLogo
-    echo
-    echo "Installing packages with Paru..."
-    echo
-
-    sudo -u "$SUDO_USER" paru -Sy --noconfirm \
-        qt5ct-kde \
-        qt6ct-kde \
-        calcure \
-        papirus-folders \
-        udev-block-notify \
-        wlr-dpms \
-        mangowm-git \
-        nmtui-go \
-        waybar-git \
-
-    paru -S --noconfirm \
-        kitty \
-        matugen \
-        libadwaita \
-        papirus-icon-theme \
-        swaylock-effects \
-        swayidle \
-        swaync \
-        awww \
-        pavucontrol \
-        blueman \
-        rofi \
-        neovim \
-        fastfetch \
-        archlinux-xdg-menu \
-        xdg-desktop-portal-wlr \
-        waypaper \
-        nwg-look \
-        nwg-clipman \
-        --overwrite "*"
-
-    if [[ $? -ne 0 ]]; then
-        echo
-        echo "Package installation failed."
-        exit 1
-    fi
-
-    clear
-    echoLogo
 }
 
 createDotfiles() {
@@ -254,9 +159,7 @@ createDotfiles() {
 
 askDotfiles() {
     clear
-    echoLogo
-    echo
-    echo "Copy the contents from the dotfiles?"
+    echo "Almost done! Do you want to copy the dotfiles?"
     echo
     echo "[y] Yes"
     echo "[n] No"
@@ -268,62 +171,17 @@ askDotfiles() {
         y|Y|yes|YES|Yes)
             createDotfiles
             echo
-            echo "Creating dotfiles done."
+            echo "It's done!"
             ;;
         n|N|no|NO|No)
             echo
-            echo "No dotfiles created."
+            echo "No dotfiles created..."
             ;;
         *)
             echo
-            echo "Wrong input."
+            echo "Oops! Wrong input!"
             sleep 1
             askDotfiles
-            ;;
-    esac
-}
-
-createBackup() {
-    local backupDir="$USER_HOME/.config/backup"
-
-    mkdir -p "$backupDir"
-
-    find "$USER_HOME/.config" \
-        -mindepth 1 \
-        -maxdepth 1 \
-        ! -name backup \
-        -exec cp -r {} "$backupDir/" \;
-
-    chown -R "$SUDO_USER:$SUDO_USER" "$backupDir"
-}
-
-askBackup() {
-    clear
-    echoLogo
-    echo
-    echo "Create a backup of .config?"
-    echo
-    echo "[y] Yes"
-    echo "[n] No"
-    echo
-
-    read -r backupChoice
-
-    case "$backupChoice" in
-        y|Y|yes|YES|Yes)
-            createBackup
-            echo
-            echo "Backup done."
-            ;;
-        n|N|no|NO|No)
-            echo
-            echo "No backup created."
-            ;;
-        *)
-            echo
-            echo "Wrong input."
-            sleep 1
-            askBackup
             ;;
     esac
 }
@@ -332,28 +190,17 @@ if command -v yay > /dev/null 2>&1; then
     yay=true
 fi
 
-if command -v paru > /dev/null 2>&1; then
-    paru=true
-fi
-
-echoLogo
-
-if [[ $yay == true && $paru == true ]]; then
+if [[ $yay == true ]]; then
     getPackageManager
 elif [[ $yay == true ]]; then
-    yayInstallPackages
-elif [[ $paru == true ]]; then
-    paruInstallPackages
+    installPackages
 else
-    installPackageManagerCheck
+    packageManagerCheck
 fi
 
 askDotfiles
-askBackup
 
 clear
-echoLogo
-echo
-echo "All done."
+echo "All done! Thank you for using my dotfiles!"
 echo
 exit 0
